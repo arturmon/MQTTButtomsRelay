948bf6dc6c5652fc8555e6ff38de8c6de40f66d2
arturmon <arturmon82@gmail.com>
2015-11-02 20:10:16 +0300

    Update MQTTButtomsRelay.ino

--- MQTTButtomsRelay/MQTTButtomsRelay.ino
+++ MQTTButtomsRelay/MQTTButtomsRelay.ino
@@ -1,120 +1,156 @@
-// sketch för a "light switch" where you can control light or something 
-// else from both vera and a local physical button (connected between digital
-// pin 4,5 and GND).
-// This node also works as a repeader for other nodes
+/**
+ * 
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * DESCRIPTION
+ *
+ * 
+ * Используется библиотека MySensors 1.5
+ * 
+ * 
+ */
 
-#include <MySensor.h>
+
+// Включить отладку
+#define MY_DEBUG 
+
+// Выбор трансорта NRF24 поумолчанию
+#define MY_RADIO_NRF24
+//#define MY_RADIO_RFM69
+
+#define MY_NODE_ID AUTO
+
+#define MY_REPEATER_FEATURE
+
 #include <SPI.h>
+#include <MySensor.h>
 #include <Bounce2.h>
 
-#define RELAY_PIN  6  // Arduino Digital I/O pin number for relay 
-#define RELAY_PIN_2  7  // Arduino Digital I/O pin number for relay 
-#define BUTTON_PIN  4  // Arduino Digital I/O pin number for button 
-#define BUTTON_PIN_2  5  // Arduino Digital I/O pin number for button 
-#define CHILD_ID 1   // Id of the sensor child
-#define CHILD_ID_2 2   // Id of the sensor child
-#define RELAY_ON 1
-#define RELAY_OFF 0
+// Пины подключения Реле
+#define RELAY_PIN_1  6  
+#define RELAY_PIN_2  7  
+// Пины подключения Выключателей
+#define BUTTON_PIN_1  4  
+#define BUTTON_PIN_2  5  
+// ID Сенсоров 
+#define CHILD_ID_1 1   
+#define CHILD_ID_2 2   
+// Какое Состояние включает и выключает реле 0 или 1
+#define RELAY_ON 0
+#define RELAY_OFF 1
 
-Bounce debouncer = Bounce(); 
-Bounce debouncer_2 = Bounce(); 
-int oldValue=0;
+Bounce debouncer_1 = Bounce(); 
+Bounce debouncer_2 = Bounce();
+int oldValue_1=0;
 int oldValue_2=0;
-bool state;
+bool state_1;
 bool state_2;
+
 MySensor gw;
-MyMessage msg(CHILD_ID,V_LIGHT);
+MyMessage msg_1(CHILD_ID_1,V_LIGHT);
 MyMessage msg_2(CHILD_ID_2,V_LIGHT);
 
 void setup()  
 {  
   gw.begin(incomingMessage, AUTO, true);
 
-  // Send the sketch version information to the gateway and Controller
-  gw.sendSketchInfo("Button & Relay", "0.1");
-
- // Setup the button
-  pinMode(BUTTON_PIN,INPUT);
+  gw.sendSketchInfo("Buttoms_Relay_v3", "1.6 29.10.2015");   
+  // Настройки Кнопки
+  pinMode(BUTTON_PIN_1,INPUT);
+  // Включение подтягивающего резистора
+  digitalWrite(BUTTON_PIN_1,HIGH);
+  // Настройки Кнопки
   pinMode(BUTTON_PIN_2,INPUT);
-  // Activate internal pull-up
-  digitalWrite(BUTTON_PIN,HIGH);
+  // Включение подтягивающего резистора
   digitalWrite(BUTTON_PIN_2,HIGH);
-  
-  // After setting up the button, setup debouncer
-  debouncer.attach(BUTTON_PIN);
+
+ 
+  // Натсройка антидребезга
+  debouncer_1.attach(BUTTON_PIN_1);
+  debouncer_1.interval(5);
+  // Натсройка антидребезга
   debouncer_2.attach(BUTTON_PIN_2);
-  debouncer.interval(5);
   debouncer_2.interval(5);
 
-  // Register all sensors to gw (they will be created as child devices)
-  gw.present(CHILD_ID, S_LIGHT);
-  gw.present(CHILD_ID_2, S_LIGHT);
 
-  // Make sure relays are off when starting up
-  digitalWrite(RELAY_PIN, RELAY_OFF);
-  digitalWrite(RELAY_PIN_2, RELAY_OFF);
-  // Then set relay pins in output mode
-  pinMode(RELAY_PIN, OUTPUT); 
-  pinMode(RELAY_PIN_2, OUTPUT);  
+  // Регистрация Датчиков
+  gw.present(CHILD_ID_1, S_LIGHT);
+  // Регистрация Датчиков
+  gw.present(CHILD_ID_2, S_LIGHT);  
+
+
+
+  // Проверяем что при включении реле выключенны
+  digitalWrite(RELAY_PIN_1, RELAY_OFF);
+  // Включаем Пин на вывод
+  pinMode(RELAY_PIN_1, OUTPUT);   
+
       
-  // Set relay to last known state (using eeprom storage) 
-  state = gw.loadState(CHILD_ID);
+  // Устанавливаем последнее состояние Реле
+  state_1 = gw.loadState(CHILD_ID_1);
+  digitalWrite(RELAY_PIN_1, state_1?RELAY_ON:RELAY_OFF);  
+
+  // Проверяем что при включении реле выключенны
+  digitalWrite(RELAY_PIN_2, RELAY_OFF);
+  // Включаем Пин на вывод
+  pinMode(RELAY_PIN_2, OUTPUT);   
+      
+  // Устанавливаем последнее состояние Реле
   state_2 = gw.loadState(CHILD_ID_2);
-  digitalWrite(RELAY_PIN, state?RELAY_ON:RELAY_OFF);
-  digitalWrite(RELAY_PIN_2, state_2?RELAY_ON:RELAY_OFF);  
+  digitalWrite(RELAY_PIN_2, state_2?RELAY_ON:RELAY_OFF);    
 }
 
 
-/*
-*  Example on how to asynchronously check for new messages from gw
-*/
 void loop() 
 {
   gw.process();
-  debouncer.update();
+  debouncer_1.update();
   debouncer_2.update();
-  // Get the update value
-  int value = debouncer.read();
-  int value_2 = debouncer_2.read();
-  if (value != oldValue && value==0) {
-      gw.send(msg.set(state?false:true), true); // Send new state and request ack back
+  // Обновляем значения
+  int value_1 = debouncer_1.read();
+  if (value_1 != oldValue_1 && value_1==0) {
+      gw.send(msg_1.set(state_1?false:true), true); // Отправляем новое значение с ACK подтверждением
   }
-  oldValue = value;
+  oldValue_1 = value_1;
+   
+  // Обновляем значения
+  int value_2 = debouncer_2.read();
   if (value_2 != oldValue_2 && value_2==0) {
-      gw.send(msg_2.set(state_2?false:true), true); // Send new state and request ack back
+      gw.send(msg_2.set(state_2?false:true), true); // Отправляем новое значение с ACK подтверждением
   }
   oldValue_2 = value_2;  
 } 
- 
+
 void incomingMessage(const MyMessage &message) {
-  // We only expect one type of message from controller. But we better check anyway.
+  // Ожидаем только один тип сообщения. Но лучше проверим.
   if (message.isAck()) {
      Serial.println("This is an ack from gateway");
   }
 
-  if (message.type == V_LIGHT) {
-    if (message.sensor == CHILD_ID){
-     // Change relay state
-     state = message.getBool();
-     digitalWrite(RELAY_PIN, state?RELAY_ON:RELAY_OFF);
-     // Store state in eeprom
-     gw.saveState(CHILD_ID, state);
-    }
-    if (message.sensor == CHILD_ID_2){
-     // Change relay state
-     state_2 = message.getBool();
-     digitalWrite(RELAY_PIN_2, state_2?RELAY_ON:RELAY_OFF);
-     // Store state in eeprom
-     gw.saveState(CHILD_ID_2, state_2);
-    }    
+  if (message.type == V_LIGHT && message.sensor == CHILD_ID_1) {
+     // Изменяем значение реле
+     state_1 = message.getBool();
+     digitalWrite(RELAY_PIN_1, state_1?RELAY_ON:RELAY_OFF);
+     // Сохраняем значение в  eeprom
+     gw.saveState(CHILD_ID_1, state_1);
     
-     // Write some debug info
      Serial.print("Incoming change for sensor:");
      Serial.print(message.sensor);
      Serial.print(", New status: ");
      Serial.println(message.getBool());
-
    } 
-
+  if (message.type == V_LIGHT && message.sensor == CHILD_ID_2) {
+     // Изменяем значение реле
+     state_2 = message.getBool();
+     digitalWrite(RELAY_PIN_2, state_2?RELAY_ON:RELAY_OFF);
+     // Сохраняем значение в  eeprom
+     gw.saveState(CHILD_ID_2, state_2);
+    
+     Serial.print("Incoming change for sensor:");
+     Serial.print(message.sensor);
+     Serial.print(", New status: ");
+     Serial.println(message.getBool());
+   }    
 }
 
